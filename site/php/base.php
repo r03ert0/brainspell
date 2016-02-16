457974ca85f624873aeace51ab8fcc6825036be0
Stefan van der Walt <stefanv@berkeley.edu>
2016-02-16 11:17:32 -0800

    Set up MySQL database correctly

--- site/php/base.php
+++ site/php/base.php
@@ -3,4 +3,5 @@ session_start();
 $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
 $dbuser = "root"; // the username that you created, or were given, to access your database
 $dbpass = "beo8hkii"; // the password that you created, or were given, to access your database
+$dbname = "brainspell";
 ?>
