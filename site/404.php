100c2b7f0f7ca030b744b0fb364b424e2f965f80
JB Poline <jbpoline@gmail.com>
2016-02-17 15:12:49 -0800

    correct to get server name for docker virtualhost

--- site/404.php
+++ site/404.php
@@ -43,7 +43,7 @@
 		$getdata = http_build_query(array("query" => $q,'action' => 'search_lucene'));
 		$opts = array('http' =>array('method'=>'POST','header'=>'Content-type: application/x-www-form-urlencoded'));
 		$context  = stream_context_create($opts);
-		$result = file_get_contents('http://'.$_SERVER['HTTP_HOST'].'/php/brainspell.php?'.$getdata, false, $context);	
+		$result = file_get_contents('http://'.$_SERVER['SERVER_NAME'].'/php/brainspell.php?'.$getdata, false, $context);
 		echo $result;	
 	}
 	else
