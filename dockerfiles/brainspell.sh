8185a72702aaf0856cc1d17ce44ff72a1bd00063
Stefan van der Walt <stefanv@berkeley.edu>
2016-02-16 16:58:48 -0800

    Set up brainspell + Apache

--- dockerfiles/brainspell.sh
+++ dockerfiles/brainspell.sh
@@ -5,5 +5,5 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir -p $SCRIPTPATH/tmp/mysql
 exec docker run --name=brainspell -p 127.0.0.1:8888:80 \
                 -v $SCRIPTPATH/tmp/mysql:/var/lib/mysql \
-                -v $SCRIPTPATH/..:/brainspell \
+                -v $SCRIPTPATH/..:/home/brainspell \
                 -t brainspell
