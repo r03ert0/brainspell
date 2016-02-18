cbe6218d5d774de3e7e77cac5696766e14a15c30
Dav Clark <davclark@gmail.com>
2016-02-17 23:08:29 -0800

    Most of the way there for docker compose
    Currently have a strange issue with the MySQL container not running...

--- dockerfiles/brainspell.sh
+++ dockerfiles/brainspell.sh
@@ -3,7 +3,7 @@ docker stop brainspell && docker rm brainspell
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir -p $SCRIPTPATH/tmp/mysql
-exec docker run --name=brainspell -p 127.0.0.1:8888:80 \
+exec docker run --name=brainspell -p 80:80 \
                 -v $SCRIPTPATH/tmp/mysql:/var/lib/mysql \
                 -v $SCRIPTPATH/..:/home/brainspell \
                 -t brainspell
